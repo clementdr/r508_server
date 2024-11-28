5859fca255ac0e62ce071d4387c6aed4c1e540dd
Clément De Roberti <cderoberti@gmail.com>
2024-11-28 10:24:57 +0100

    first commit

---
+++ app/api/todos/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+const todos = [{ text: "Apprendre Next" }, { text: "Créer une API" }];
+
+export async function GET(req) {
+  return NextResponse.json(todos, { status: 200 });
+}
+
+export async function POST(req) {
+  const todo = await req.json();
+
+  todos.push(todo);
+
+  return NextResponse.json(todos, { status: 200 });
+}
