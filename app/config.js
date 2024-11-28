96820e8434b54c1001d526ac616e2fccc70a9d66
Clément De Roberti <cderoberti@gmail.com>
2024-11-28 10:49:20 +0100

    push

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 export function getBaseUrl() {
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return "https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}";
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
   return "http://localhost:3000";
 }
